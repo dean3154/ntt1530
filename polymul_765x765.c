7085a67e6fb68a4812964e1f6cb6b464c87c3890
dean3154 <50229236+dean3154@users.noreply.github.com>
2020-08-03 01:20:24 +0800

    Add files via upload

--- polymul_765x765.c
+++ polymul_765x765.c
@@ -5,15 +5,17 @@ extern void fft9(void* fpad);
 extern void polymul_10x10_153(void* fpad, void* gpad);
 extern void ifft9(void* fpad);
 extern void ifft17(void* fpad, void* h);
+extern void mod_reduce(void* h, void* hpad);
 // input  : int16_t f[765], int16_t g[765]
 // output : int16_t h[1530], h[0]~h[1528] = f * g, h[1529] = 0
 void polymul_765x765(int16_t* h, int16_t* f, int16_t* g){
-    int16_t fpad[1530], gpad[1530];
+    int16_t fpad[1530], gpad[1530], hpad[1530];
     fft17(f, fpad);
     fft9(fpad);
     fft17(g, gpad);
     fft9(gpad);
     polymul_10x10_153(fpad, gpad);
     ifft9(fpad);
-    ifft17(fpad, h);
+    ifft17(fpad, hpad);
+    mod_reduce(h, hpad);
 }
