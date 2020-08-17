a6416de816d126f2922efe97dfc27ff23575f584
dean3154 <50229236+dean3154@users.noreply.github.com>
2020-08-17 10:41:07 +0800

    Add files via upload

--- polymul_761x761_modp.c
+++ polymul_761x761_modp.c
@@ -4,8 +4,10 @@ extern void fft17(void* f, void* fpad);
 extern void fft17_byte(void* g, void* gpad);
 extern void fft9(void* fpad);
 extern void polymul_10x10_153(void* fpad, void* gpad);
+extern void polymul_10x10_153_mr(void* fpad, void* gpad);
 extern void ifft9(void* fpad);
 extern void ifft17(void* fpad, void* h);
+extern void ifft17_mr(void* fpad, void* h);
 extern void mod_reduce(void* h, void* hpad);
 // input  : int16_t f[761], int8_t g[761]
 // output  : int16_t h[761]
@@ -16,8 +18,10 @@ void polymul_761x761_modp(int16_t* h, int16_t* f, int8_t* g){
     fft9(fpad);
     fft17_byte(g, gpad);
     fft9(gpad);
-    polymul_10x10_153(fpad, gpad);
+    // polymul_10x10_153(fpad, gpad);
+    polymul_10x10_153_mr(fpad, gpad);
     ifft9(fpad);
-    ifft17(fpad, gpad);
+    // ifft17(fpad, gpad);
+    ifft17_mr(fpad, gpad);
     mod_reduce(h, gpad);
 }
